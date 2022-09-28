8d8dc890e13488f872295c37d3e5f99157c3899e
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-09-28 21:56:32 +0100

    first commit

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BinarySearch {
+    private int start;
+    private int end;
+    private int midPoint;
+
+    public static void main(String... args) {
+        int colors = 0;
+        List<Integer> list = new ArrayList<>(Arrays.asList(colors));
+        int target = 67;
+    }
+    public static int binarySearch(int start, int end, int midPoint){
+        return 0;
+    }
+}
